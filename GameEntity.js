7f5a68a27b031b873404bed871e8f2cd46bad054
Revtrov <73724492+Revtrov@users.noreply.github.com>
2023-04-21 12:33:41 +0100

    Update GameEntity.js

--- GameEntity.js
+++ GameEntity.js
@@ -1,31 +1,64 @@
-import {Buffer} from "./Buffer.js"
+import { Buffer } from "./Buffer.js";
 class GameEntity {
-    
-  constructor(_spriteSrc, _x, _y, _width, _height,_zHeight,_controllable) {
-    const imageLoader = document.createElement('canvas');
-    const imageLoaderCtx = imageLoader.getContext("2d");
+  constructor(
+    _spriteSrc,
+    _x,
+    _y,
+    _width,
+    _height,
+    _zHeight,
+    _rotation,
+    _controllable
+  ) {
+    this.imageLoader = document.createElement("canvas");
+    this.imageLoaderCtx = this.imageLoader.getContext("2d");
     this.x = _x;
     this.y = _y;
+    this.rotation = _rotation;
     this.width = _width;
     this.height = _height;
-    imageLoader.height = this.height;
-    imageLoader.width = this.width;
+    this.imageLoader.height = this.height;
+    this.imageLoader.width = this.width;
     this.zHeight = _zHeight;
-    this.buffer = new Buffer(this.width,this.height);
+    this.buffer = new Buffer(this.width, this.height);
     this.sprite = new Image();
     this.sprite.src = _spriteSrc;
     let imageData;
-    imageLoaderCtx.imageSmoothingEnabled = false;
+    this.imageLoaderCtx.imageSmoothingEnabled = false;
     this.sprite.onload = () => {
-      imageLoader.imageSmoothingEnabled = false;
-      imageLoaderCtx.drawImage(this.sprite, 0, 0, this.width, this.height);
-      imageData = imageLoaderCtx.getImageData(0, 0, this.width, this.height);
+      this.imageLoader.imageSmoothingEnabled = false;
+      this.imageLoaderCtx.drawImage(this.sprite, 0, 0, this.width, this.height);
+      imageData = this.imageLoaderCtx.getImageData(
+        0,
+        0,
+        this.width,
+        this.height
+      );
       this.buffer.data = imageData.data;
     };
   }
-  updateEntity(buffer){
-    buffer.merge(this.buffer,~~this.x,~~this.y)
+  updateEntity(buffer) {
+    buffer.merge(this.buffer, ~~this.x, ~~this.y);
   }
-
+  rotateEntity(deg) {
+    let imageData;
+      this.imageLoader.imageSmoothingEnabled = false;
+      this.imageLoaderCtx.save();
+      var rad = (deg * Math.PI) / 180;
+      this.imageLoaderCtx.translate(this.width/2,this.height/2);
+      this.imageLoaderCtx.rotate(rad);
+      
+    this.imageLoaderCtx.translate(-this.width/2, -this.height/2);
+      this.imageLoaderCtx.drawImage(this.sprite, 0, 0, this.width, this.height);
+      this.imageLoaderCtx.restore();
+      imageData = this.imageLoaderCtx.getImageData(
+        0,
+        0,
+        this.width,
+        this.height
+      );
+      //this.buffer.data = []
+      this.buffer.data = imageData.data;
+  }
 }
 export { GameEntity };
